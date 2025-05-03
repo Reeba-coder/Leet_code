2bb4129ab98313ab571452c9aed54df78d48bcac
Reeba <reebarasheedahmed@gmail.com>
2025-05-03 10:10:16 +0500

    Add files via upload

---
+++ 15th.py
@@ -0,0 +1,10 @@
+def checkStraightLine(coordinates):
+    x0,y0=coordinates[0]
+    x1,y1=coordinates[1]
+    for i in range(2 , len(coordinates)):
+        x,y=coordinates[i]
+        if (y1-y0)*(x-x0)!=(y-y0)*(x1-x0):
+            return False
+    return True
+print(checkStraightLine([[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]))
+print(checkStraightLine([[1,1],[2,2],[3,3],[4,4],[5,5],[6,7]]))
